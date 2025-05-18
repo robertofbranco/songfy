258d4591b4a211d0233d44866b9e297f3af6b4f5
bettoh <bebeto.fb@hotmail.com>
2025-05-18 20:28:23 +0200

    Creates index page and simple flow

---
+++ app/static/index.js
@@ -0,0 +1,17 @@
+window.onload = (e) => {
+    document.getElementById('start-game-btn').addEventListener('click', startGame);
+}
+
+function startGame() {
+    const playlistLink = document.getElementById('playlist-input').value
+    // if (playlistLink === "") {
+    //
+    // }
+
+    const playlistId = playlistLink.split('https://open.spotify.com/playlist/')[1].split('?')[0];
+    // if (playlistId.length === 0) {
+    //
+    // }
+
+    window.location.href = `/songfy/game?playlistId=${playlistId}`;
+}
