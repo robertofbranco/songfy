b4b5cf3693d96b392f1b58e0f6ea3f02451d696d
bettoh <bebeto.fb@hotmail.com>
2026-07-18 23:57:12 +0200

    Implement deduplication on the songs list

--- app/spotify_service.py
+++ app/spotify_service.py
@@ -24,12 +24,18 @@ class SpotifyService:
                                                               client_secret=SpotifyService.CLIENT_SECRET)
         sp = Spotify(client_credentials_manager=client_credentials_manager)
         songs_found: list[Song] = []
+        seen_song_ids: set[str] = set()
 
         playlist = sp.playlist_items(playlist_id)["items"]
         for item in playlist:
             song = item["track"]
             song_name = song['name']
             song_id = song['id']
+
+            if song_id in seen_song_ids:
+                continue
+
+            seen_song_ids.add(song_id)
             song_artists = song['artists']
             song_artists_names = [artist["name"] for artist in song_artists]
             song_year = song['album']['release_date']
